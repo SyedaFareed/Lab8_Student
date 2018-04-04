cda2c4926f817e5e8bd6627d8c63908fa2b6fa17
SyedaFareed <14besesfareed@seecs.edu.pk>
2018-04-04 11:59:21 +0500

    Add files via upload

---
+++ Db.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Random;
+
+/**
+ *
+ * @author Dell
+ */
+public class Db {
+    Connection con= null;
+    Statement stmt= null;
+    PreparedStatement preps= null;
+    Random rand;
+    
+    Db(){
+      try{
+	      //Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Open a connection
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cafe", "root", "root");
+                
+                //con.close(); 
+      }
+            
+      catch(Exception i){
+          System.out.println(i);    //print exception
+      }
+      
+      rand = new Random();
+    }   //end constructor 
+    
+    public void insert_stmt(Boolean x){
+
+        
+        try{
+            con.setAutoCommit(x);
+            stmt = con.createStatement();
+            Boolean ret;
+            int temp;
+
+            String query= "";
+            int  regno = 0;
+
+            for(int i=0; i<5000; i++){
+                regno = rand.nextInt(5000) + 1;  
+                query= "INSERT INTO `lab8_student`.`students` (`regno`, `name`) VALUES ('"+regno+"', 'a"+regno+"')";
+                ret = stmt.execute(query);
+
+            }
+            
+            if(x==false)
+                con.commit();
+            
+            stmt.close();
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+        finally{
+      //finally block used to close resources
+          try{
+             if(stmt!=null)
+                stmt.close();
+             if(con!=null)
+                con.close();
+          }catch(Exception se){
+             se.printStackTrace();}
+      }//end finally try
+    }   //end function
+    
+    
+    
+    
+    
+    
+    
+    public void insert_preps(Boolean x){
+        try{
+            con.setAutoCommit(x);
+            
+            stmt = con.createStatement();
+            Boolean ret;
+            int temp;
+
+            String query= "";
+            int  regno = 0;
+
+            for(int i=0; i<5000; i++){
+                regno = rand.nextInt(5000) + 1;  
+
+                query= "INSERT INTO `lab8_student`.`students` (`regno`, `name`) VALUES (?,?)";
+                preps = con.prepareStatement(query);
+                preps.setInt(1, regno);  // This would set age
+                preps.setString(2, "a"+regno); // This would set ID
+                temp = preps.executeUpdate();
+
+            }
+            
+            if(x==false)
+                con.commit();            
+            
+            preps.close();    
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+        finally{
+      //finally block used to close resources
+          try{
+              if(preps!=null)
+                preps.close();
+             if(con!=null)
+                con.close();
+          }catch(Exception se){
+             se.printStackTrace();}
+      }//end finally try
+    }   //end function
+    
+    
+    
+    public void insert_batch(Boolean x){
+
+        try{
+            
+            con.setAutoCommit(x);            
+            stmt = con.createStatement();
+            Boolean ret;
+
+            String query= "";
+            int  regno = 0;
+
+            for(int i=0; i<5000; i++){
+                regno = rand.nextInt(5000) + 1;  
+                query= "INSERT INTO `lab8_student`.`students` (`regno`, `name`) VALUES ('"+regno+"', 'a"+regno+"')";
+                stmt.addBatch(query);
+
+            }
+            
+            int[] count = stmt.executeBatch();
+            
+            if(x==false)
+                con.commit();
+            
+            stmt.close();
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+        finally{
+      //finally block used to close resources
+          try{
+             if(stmt!=null)
+                stmt.close();
+          }catch(Exception se){
+             se.printStackTrace();}
+      }//end finally try
+    }   //end function    
+    
+
+}
---
+++ Lab8_Student.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_student;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class Lab8_Student {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Db d= new Db();
+        
+        //d.insert_stmt(false);
+        //d.insert_stmt(true);
+        
+        //d.insert_preps(false);
+        d.insert_preps(true);
+        
+        //d.insert_batch(false);
+        //d.insert_batch(true);
+    }
+    
+}
